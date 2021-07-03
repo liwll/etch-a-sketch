0205444c030c330639b59c2a18c66e1474e68630
liwll <liwill7778@gmail.com>
2021-07-03 13:39:06 -0400

    Implemented ability to change the size of the sketchpad using a slider

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 function createGrid(n) {
-    const container = document.querySelector(".container");
+    const container = document.querySelector('.container');
+    while(container.firstChild != null) {
+        container.removeChild(container.lastElementChild);
+    }
+
     for (let i = 0; i < n**2; i++) {
         const gridBox = document.createElement('div');
         gridBox.classList.add('grid-box');
         container.appendChild(gridBox);
-        gridBox.addEventListener("mouseenter", () => {
-            gridBox.style.backgroundColor = "aqua";
+        gridBox.addEventListener('mouseenter', () => {
+            gridBox.style.backgroundColor = 'aqua';
         });
         let width = 100/n;
-        gridBox.setAttribute("style", `width:${width}%; height:${width}%`);
+        gridBox.setAttribute('style', `width:${width}%; height:${width}%`);
     }
 }
 
-createGrid(12);
-
-const gridBoxes = document.querySelectorAll('.grid-box');
-
 function clearGrid() {
+    const gridBoxes = document.querySelectorAll('.grid-box');
+
     gridBoxes.forEach(gridBox => {
-        gridBox.style.backgroundColor = "white";
+        gridBox.style.backgroundColor = 'white';
     })
 }
 
-const clearButton = document.querySelector('.clear-grid-button');
+function newGrid(size) {
+    createGrid(size)
+}
 
-clearButton.addEventListener("click", () => {
+const clearButton = document.querySelector('.clear-grid-button');
+clearButton.addEventListener('click', () => {
     clearGrid();
 });
 
+function updateSizeText(size) {
+    const sizeText = document.querySelector('.size-text');
+    sizeText.textContent = `New Size: ${size} x ${size}`;
+}
+
+function updateCurrentText(size) {
+    const currentText = document.querySelector('.current-size-text');
+    
+    currentText.textContent = `Current Size: ${size} x ${size}`;
+}
+
+const submitSize = document.querySelector('.submit-size');
+submitSize.addEventListener('click', () => {
+    const newSize = document.querySelector('.new-size');
+    newGrid(newSize.value);
+    updateCurrentText(newSize.value);
+})
+
+
+createGrid(12);
 
 
